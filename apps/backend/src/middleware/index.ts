f32bdf9478593f60dcd195a8c03555ac7ffc0382
Facundo Fernández <facuu201202@gmail.com>
2026-02-18 18:05:34 -0300

    feat: configure CORS middleware with specific origin and credentials

--- apps/backend/src/middleware/index.ts
+++ apps/backend/src/middleware/index.ts
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import { Application, NextFunction, Request, Response } from "express";
 
 export const setupMiddleware = (app: Application): void => {
-  app.use(cors());
+  app.use(cors(
+    { origin: "http://localhost:5173",
+      credentials: true
+    }
+  ));
   app.use(bodyParser.json());
   
   app.use((req: Request, res: Response, next: NextFunction) => {
