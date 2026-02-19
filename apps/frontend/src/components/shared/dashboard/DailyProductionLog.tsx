776b37de78513241ae054f699a37d7439d8dfc3e
Nievas2 <angelgabrielnievas@gmail.com>
2026-02-19 18:06:13 -0300

    feat(Dashboard): :sparkles: add success modal

--- apps/frontend/src/components/shared/dashboard/DailyProductionLog.tsx
+++ apps/frontend/src/components/shared/dashboard/DailyProductionLog.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/src/components/common/button'
 import { Card, CardContent, CardHeader } from '@/src/components/common/card'
-import { AlertCircle, ArrowRight, Plus } from 'lucide-react'
+import { AlertCircle, ArrowRight, Grid, Plus } from 'lucide-react'
 import {
   Table,
   TableBody,
@@ -30,8 +30,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/src/components/common/select'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const DailyProductionLog = () => {
+  const [open, setOpen] = useState(false)
+  const [finished, setFinished] = useState(false)
   return (
     <Card>
       <CardHeader className="flex items-center justify-between">
@@ -41,72 +45,123 @@ const DailyProductionLog = () => {
         </div>
 
         <div className="flex justify-end">
-          <Dialog>
+          <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
               <Button variant="outline" size="sm">
                 Crear lote
                 <Plus className="ml-2 size-4" />
               </Button>
             </DialogTrigger>
-            <DialogContent>
-              <DialogHeader className="border-b p-2">
-                <DialogTitle className="text-[32px] font-bold text-black">
-                  Crear nuevo lote
-                </DialogTitle>
+            {finished ? (
+              <DialogContent className="space-y-6">
+                <DialogHeader>
+                  <DialogTitle className="text-[32px] font-bold text-black flex justify-center">
+                    <img src="/successIcon.svg" alt="success" />
+                  </DialogTitle>
 
-                <DialogDescription>
-                  Ingresa los datos para iniciar el seguimiento de producción.
-                </DialogDescription>
-              </DialogHeader>
+                  <DialogTitle className="text-[32px] font-bold text-black flex justify-center">
+                    Lote creado correctamente
+                  </DialogTitle>
 
-              <form className="space-y-6">
-                <div className="space-y-4">
-                  <Label className="font-bold">Fecha de producción</Label>
-                  <Input type="date" placeholder="dd/mm/aaaa" />
-                </div>
+                  <DialogDescription className="text-center text-[16px]">
+                    El nuevo lote ha sido registrado exitosamente en <br />
+                    el sistema. Ahora puedes gestionar su <br />
+                    seguimiento y produccion.
+                  </DialogDescription>
+                </DialogHeader>
 
-                <div className="space-y-4">
-                  <Label className="font-bold">Tipo de producción</Label>
-                  <Select>
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Selecciona producto..." />
-                    </SelectTrigger>
-
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem value="queso-fresco">
-                          Queso fresco
-                        </SelectItem>
-                        <SelectItem value="queso-duro">Queso duro</SelectItem>
-                        <SelectItem value="leche-cruda">Leche cruda</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-
-                <div className="space-y-4">
-                  <Label className="font-bold">
-                    Cantidad producida (Kg/Litros)
-                  </Label>
-                  <Input type="text" inputMode="decimal" placeholder="0.00" />
-                </div>
-
-                <span className="flex items-center gap-2 text-xs">
-                  <AlertCircle className="size-5" /> Verifica que los datos sean
-                  correctos antes de crear el lote.
-                </span>
-                <DialogFooter>
+                <div className="p-4 space-y-4">
                   <Button
                     variant="default"
-                    className="flex items-center justify-center w-full h-16 text-xl font-bold"
-                    type="submit"
+                    className="flex items-center justify-center w-full h-14 text-xl font-bold"
+                    asChild
                   >
-                    Crear lote
-                    <ArrowRight className="size-6" />
+                    <Link to="/dashboard" className="block">
+                      Ir al detalle del lote
+                      <ArrowRight className="ml-2 size-6" />
+                    </Link>
+                  </Button>
+
+                  <Button
+                    variant="secondary"
+                    className="flex items-center justify-center w-full h-14 text-xl font-bold"
+                    onClick={() => setFinished(false)}
+                  >
+                    Crear otro lote
+                  </Button>
+                </div>
+
+                <DialogFooter className="flex flex-row justify-center sm:justify-center items-center text-center">
+                  <Button variant="ghost" onClick={() => setOpen(false)}>
+                    <Grid className="size-5" />
+                    <span className="underline">Volver al dashboard</span>
                   </Button>
                 </DialogFooter>
-              </form>
-            </DialogContent>
+              </DialogContent>
+            ) : (
+              <DialogContent>
+                <DialogHeader className="border-b p-2">
+                  <DialogTitle className="text-[32px] font-bold text-black">
+                    Crear nuevo lote
+                  </DialogTitle>
+
+                  <DialogDescription>
+                    Ingresa los datos para iniciar el seguimiento de producción.
+                  </DialogDescription>
+                </DialogHeader>
+
+                <form className="space-y-6">
+                  <div className="space-y-4">
+                    <Label className="font-bold">Fecha de producción</Label>
+                    <Input type="date" placeholder="dd/mm/aaaa" />
+                  </div>
+
+                  <div className="space-y-4">
+                    <Label className="font-bold">Tipo de producción</Label>
+                    <Select>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Selecciona producto..." />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="queso-fresco">
+                            Queso fresco
+                          </SelectItem>
+                          <SelectItem value="queso-duro">Queso duro</SelectItem>
+                          <SelectItem value="leche-cruda">
+                            Leche cruda
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="space-y-4">
+                    <Label className="font-bold">
+                      Cantidad producida (Kg/Litros)
+                    </Label>
+                    <Input type="text" inputMode="decimal" placeholder="0.00" />
+                  </div>
+
+                  <span className="flex items-center gap-2 text-xs">
+                    <AlertCircle className="size-5" /> Verifica que los datos
+                    sean correctos antes de crear el lote.
+                  </span>
+                  <DialogFooter>
+                    <Button
+                      variant="default"
+                      className="flex items-center justify-center w-full h-16 text-xl font-bold"
+                      type="submit"
+                      onClick={() => setFinished(true)}
+                    >
+                      Crear lote
+                      <ArrowRight className="size-6" />
+                    </Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            )}
           </Dialog>
         </div>
       </CardHeader>
