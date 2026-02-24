793aee16941580e87a7097c0e9bc1c507d03acf8
Nievas2 <angelgabrielnievas@gmail.com>
2026-02-24 11:14:15 -0300

    feat(Register): :sparkles: add register validations. add styles. add send mail styles

--- apps/frontend/src/utils/api/auth.api.ts
+++ apps/frontend/src/utils/api/auth.api.ts
@@ -1,5 +1,9 @@
+import { RegisterData } from '@/src/types/register'
+import { LoginData } from '@/src/types/login'
 import { api } from '@/src/services/api'
 
-export const registerUser = (dto: any) => api.post('/auth/crear-cuenta', dto)
+export const registerUser = (dto: RegisterData) =>
+  api.post('/auth/crear-cuenta', dto)
 
-export const loginUser = (dto: any) => api.post('/auth/iniciar-sesion', dto)
+export const loginUser = (dto: LoginData) =>
+  api.post('/auth/iniciar-sesion', dto)
