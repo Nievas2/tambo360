28f449ca72027bef453f8782e208906773e59117
Nievas2 <angelgabrielnievas@gmail.com>
2026-02-24 20:18:09 -0300

    feat(Alerts): :sparkles: add alert card

--- apps/frontend/src/components/shared/dashboard/AlertsSection.tsx
+++ apps/frontend/src/components/shared/dashboard/AlertsSection.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@/src/components/common/Button'
-import { Card, CardHeader, CardTitle } from '@/src/components/common/card'
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/src/components/common/card'
+import AlertCard from '@/src/components/shared/dashboard/AlertCard'
+import { useAuth } from '@/src/context/AuthContext'
 import { CircleAlert } from 'lucide-react'
 
 const AlertsSection = () => {
+  const { user } = useAuth()
   return (
-    <Card className="w-full max-w-72">
+    <Card className="w-full md:max-w-72 max-w-none">
       <CardHeader className="flex items-center justify-between w-full">
         <CardTitle className="flex items-center gap-2 font-bold">
           <CircleAlert /> Alertas
@@ -14,6 +22,19 @@ const AlertsSection = () => {
           Ver todas
         </Button>
       </CardHeader>
+
+      {/* Cuando no hay alertas */}
+      <CardContent className="bg-[#eaeaea] rounded-lg space-y-4 mx-4 p-4">
+        <p className="text-[14px]">
+          Hola <b>{user.nombre.split(' ')[0]}</b>, soy TamboEngine.
+        </p>
+        <p className="text-[14px]">
+          Analizare tu producción en búsqueda de anomalías, tendencias de mermas
+          y oportunidades de optimización una vez que comiences a cargar datos.
+        </p>
+      </CardContent>
+
+      <AlertCard />
     </Card>
   )
 }
