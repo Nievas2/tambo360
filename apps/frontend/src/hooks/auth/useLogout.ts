99cc5050e6bd46402826249c490b5d58a8b5cde9
Nievas2 <angelgabrielnievas@gmail.com>
2026-03-09 16:18:58 -0300

    fix(Logout): :bug: fix clean cache

--- apps/frontend/src/hooks/auth/useLogout.ts
+++ apps/frontend/src/hooks/auth/useLogout.ts
@@ -1,5 +1,5 @@
 import { logOut } from '@/src/utils/api/auth.api'
-import { queryKeys } from '@/src/utils/queryKeys'
+import { baseKeys, queryKeys } from '@/src/utils/queryKeys'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 export function useLogout() {
@@ -12,6 +12,24 @@ export function useLogout() {
 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: queryKeys.auth.currentUser })
+      queryClient.invalidateQueries({
+        queryKey: [...baseKeys.alert, 'filters'],
+      })
+      queryClient.invalidateQueries({ queryKey: queryKeys.alert.lasts() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.alert.noViewed() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.alert.lists() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.batch.lists() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.batch.day() })
+      queryClient.invalidateQueries({
+        queryKey: [...baseKeys.batch, 'filters'],
+      })
+      queryClient.invalidateQueries({ queryKey: [...baseKeys.batch, 'detail'] })
+      queryClient.invalidateQueries({ queryKey: queryKeys.cost.lists() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.decrease.lists() })
+      queryClient.invalidateQueries({ queryKey: queryKeys.dashboard.current() })
+      queryClient.invalidateQueries({
+        queryKey: [...baseKeys.dashboard, 'graph'],
+      })
     },
   })
 }
